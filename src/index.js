b45349ae3be5c9d735a78f372fc4f7b388b49372
Thallentos <thallescmarques@gmail.com>
2025-06-01 22:14:34 -0300

    começando a modelar a lógica do game

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const player1 = {
+  NOME: "Mario",
+  VELOCIDADE: 4,
+  MANOBRABILIDADE: 3,
+  PODER: 3,
+  PONTOS: 0,
+};
+
+const player2 = {
+  NOME: "Luigi",
+  VELOCIDADE: 3,
+  MANOBRABILIDADE: 4,
+  PODER: 4,
+  PONTOS: 0,
+};
+
+async function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+(async function main() {
+  console.log("Welcome to the Mario Kart game!");
+})();
