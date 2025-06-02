924785c26f6c6fa0146a230c82c4f4ce2d35a0c3
Thallentos <thallescmarques@gmail.com>
2025-06-01 23:24:03 -0300

    continuando a implementar a lógica do jogo

--- src/index.js
+++ src/index.js
@@ -18,6 +18,62 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomBlock() {
+  let random = Math.random();
+  let result;
+
+  switch (true) {
+    case random < 0.33:
+      result = "RETA";
+      break;
+
+    case random < 0.66:
+      result = "CURVA";
+      break;
+
+    default:
+      result = "CONFRONTO";
+  }
+
+  return result;
+}
+
+async function playRaceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🏁 Rodada ${round}!!!`);
+    //sortear bloco
+
+    let block = await getRandomBlock();
+    console.log(`Bloco: ${block}`);
+  }
+
+  //rolar os dados
+  let diceResult1 = await rollDice();
+  let diceResult2 = await rollDice();
+
+  //teste de habilidade
+  let totalTestSkill1 = 0;
+  let totalTestSkill2 = 0;
+
+  if (bloco === "RETA") {
+    totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
+    totalTestSkill2 = diceResult2 + character2.VELOCIDADE;
+  }
+  if (bloco === "CURVA") {
+    totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
+    totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
+  }
+  if (bloco === "CONFRONTO") {
+    totalTestSkill1 = diceResult1 + character1.PODER;
+    totalTestSkill2 = diceResult2 + character2.PODER;
+  }
+}
+
 (async function main() {
-  console.log("Welcome to the Mario Kart game!");
+  console.log("\nBem-vindo à corrida Mario Kart!");
+  console.log(
+    `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando....\nEu sou...a velocidade!`
+  );
+
+  await playRaceEngine(player1, player2);
 })();
