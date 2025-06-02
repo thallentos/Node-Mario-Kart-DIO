47ad522e812ae605e78e833907b878b0365fb953
Thallentos <thallescmarques@gmail.com>
2025-06-02 02:41:59 -0300

    apenas formatando o código

--- src/index.js
+++ src/index.js
@@ -155,16 +155,14 @@ async function declareWinner(character1, character2) {
   console.log("Resultado final:");
   console.log(`${character1.NOME}: ${character1.PONTOS} ponto(s)`);
   console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)`);
-  
+
   if (character1.PONTOS > character2.PONTOS) {
     console.log(`\n${character1.NOME} venceu a corrida! Parabéns!🏆`);
   } else if (character2.PONTOS > character1.PONTOS) {
     console.log(`\n${character2.NOME} venceu a corrida! Parabéns!🏆`);
   } else {
     console.log("A corrida terminou em empate!");
-    
   }
-  
 }
 
 (async function main() {
